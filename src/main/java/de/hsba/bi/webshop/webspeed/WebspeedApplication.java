d437f7321b62ed6006777dd82e10a7d5f1f532e6
BVSTIXI <sebastian.lieb@myhsba.de>
2022-07-04 19:42:43 +0200

    Initial commit

---
+++ src/main/java/de/hsba/bi/webshop/webspeed/WebspeedApplication.java
@@ -0,0 +1,13 @@
+package de.hsba.bi.webshop.webspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebspeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebspeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/hsba/bi/webshop/webspeed/WebspeedApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsba.bi.webshop.webspeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebspeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
