4df26d01f3cd38cfc102300ff1086ef7f7e4780c
BVSTIXI <sebastian.lieb@myhsba.de>
2022-07-04 20:09:43 +0200

    Grundlagen

--- src/main/java/de/hsba/bi/webshop/webspeed/web/IndexController.java
+++ src/main/java/de/hsba/bi/webshop/webspeed/web/IndexController.java
@@ -1,4 +1,12 @@
 package de.hsba.bi.webshop.webspeed.web;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+    public class IndexController {
+        @GetMapping("/")
+        public String index(){
+            return "index";
+        }
 }
