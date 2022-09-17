5a4034e0f0e059505dc5dedc2c1654b8900c8e40
BVSTIXI <sebastian.lieb@myhsba.de>
2022-09-17 14:48:45 +0200

    productEdit.html - Übernahme von Daten v1.0

--- src/main/java/de/hsba/bi/webshop/webspeed/product/TestProductCreator.java
+++ src/main/java/de/hsba/bi/webshop/webspeed/product/TestProductCreator.java
@@ -27,9 +27,9 @@ public class TestProductCreator {
         User testUser1 = userRepository.save(new User("Jakob","TestUser1","$2a$12$k20SYS5padgkkHZnvAOwK.PFP4ZgrbzvwHLRF76o3QXykr55MyNWO"));
         User testUser2 = userRepository.save(new User("Niklas","TestUser2","$2a$12$k20SYS5padgkkHZnvAOwK.PFP4ZgrbzvwHLRF76o3QXykr55MyNWO"));
 
-        Product testprodukt1 = productRepository.save(new Product("Katze", new BigDecimal(1000), "graue Katze, stinkt ein wenig", "Haustiere", "gut", new Double(1), new Double(12), testUser1));
-        Product testprodukt2 = productRepository.save(new Product("Hund", new BigDecimal(300), "schwarzer Hund", "Haustiere", "gebraucht", new Double(1), new Double(0), testUser1));
-        Product testprodukt3 = productRepository.save(new Product("Golf Cabrio", new BigDecimal(10000), "Neuwagen, 100k Kilometer", "Autos", "Neu", new Double(6), new Double(5), testUser2));
+        Product testprodukt1 = productRepository.save(new Product("Katze", new BigDecimal(1000), "graue Katze, stinkt ein wenig", "Haustiere", "sehr gut", new Double(1), new Double(12), testUser1));
+        Product testprodukt2 = productRepository.save(new Product("Hund", new BigDecimal(300), "schwarzer Hund", "Haustiere", "unterdurchschnittlich", new Double(1), new Double(0), testUser1));
+        Product testprodukt3 = productRepository.save(new Product("Golf Cabrio", new BigDecimal(10000), "Neuwagen, 100k Kilometer", "Autos", "unterdurchschnittlich", new Double(6), new Double(5), testUser2));
 
 
         Sale testSale1 = saleRepository.save(new Sale(testUser2, testprodukt1, 12, false));
